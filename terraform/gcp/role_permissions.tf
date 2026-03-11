c12f6a32f9e3e71888a03b0d50b27e5d70c09d7d
Timothy Colbert <s3than@users.noreply.github.com>
2026-03-11 17:26:29 +1000

    S3than pla 994 cloud permissions csi snapshots (#9)

--- terraform/gcp/role_permissions.tf
+++ terraform/gcp/role_permissions.tf
@@ -13,7 +13,10 @@ locals {
       "compute.backendServices.getEffectiveSecurityPolicies",
       "compute.backendServices.list",
       "compute.backendServices.update",
-      "compute.disks.create",
+      "compute.disks.get",  # CSI functionality
+      "compute.disks.create",  # CSI functionality
+      "compute.disks.createSnapshot",  # CSI functionality
+      "compute.disks.setLabels",  # CSI functionality
       "compute.firewalls.create",
       "compute.firewalls.delete",
       "compute.firewalls.get",
@@ -54,6 +57,12 @@ locals {
       "compute.routers.create",
       "compute.routers.delete",
       "compute.routers.get",
+      "compute.snapshots.get",  # CSI functionality
+      "compute.snapshots.list",  # CSI functionality
+      "compute.snapshots.create",  # CSI functionality
+      "compute.snapshots.useReadOnly",  # CSI functionality
+      "compute.snapshots.delete",  # CSI functionality
+      "compute.snapshots.setLabels",  # CSI functionality
       "compute.subnetworks.create",
       "compute.subnetworks.delete",
       "compute.subnetworks.get",
@@ -65,6 +74,7 @@ locals {
       "compute.targetPools.setSecurityPolicy",
       "compute.zoneOperations.get",
       "compute.zones.list",
+      "compute.zones.get",
       "iam.serviceAccounts.actAs",
       "resourcemanager.projects.get",
       "storage.buckets.create",
@@ -192,4 +202,5 @@ locals {
       "iam.serviceAccounts.signBlob",
     ]
   }
+
 }
